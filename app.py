509fc8b80f7b453aaaa7c2362271c2366206af60
joe-stifler <joseribeiro1017@gmail.com>
2024-04-24 02:28:05 +0100

    feat: enable back again the password

--- app.py
+++ app.py
@@ -22,43 +22,43 @@ import streamlit as st
 os.environ["REPLICATE_API_TOKEN"] = st.secrets["IMAGE_GENERATION"]["REPLICATE_API_TOKEN"]
 REPLICATE_MODEL_ENDPOINTSTABILITY = st.secrets["IMAGE_GENERATION"]["REPLICATE_MODEL_ENDPOINTSTABILITY"]
 
-# def check_password():
-#     """Returns `True` if the user had a correct password."""
+def check_password():
+    """Returns `True` if the user had a correct password."""
 
-#     def login_form():
-#         """Form with widgets to collect user information"""
-#         with st.form("Credentials"):
-#             st.text_input("Username", key="username")
-#             st.text_input("Password", type="password", key="password")
-#             st.form_submit_button("Log in", on_click=password_entered)
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
 
-#     def password_entered():
-#         """Checks whether a password entered by the user is correct."""
-#         if st.session_state["username"] in st.secrets[
-#             "passwords"
-#         ] and hmac.compare_digest(
-#             st.session_state["password"],
-#             st.secrets.passwords[st.session_state["username"]],
-#         ):
-#             st.session_state["password_correct"] = True
-#             del st.session_state["password"]  # Don't store the username or password.
-#             del st.session_state["username"]
-#         else:
-#             st.session_state["password_correct"] = False
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
 
-#     # Return True if the username + password is validated.
-#     if st.session_state.get("password_correct", False):
-#         return True
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
 
-#     # Show inputs for username + password.
-#     login_form()
-#     if "password_correct" in st.session_state:
-#         st.error("😕 User not known or password incorrect")
-#     return False
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
 
 
-# if not check_password():
-#     st.stop()
+if not check_password():
+    st.stop()
 
 ########################################################################################
 
