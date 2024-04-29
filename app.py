b2d31d3ddfe404c89e2d93cb8c09843fb362ea03
joe-stifler <joseribeiro1017@gmail.com>
2024-04-29 23:21:58 +0100

    feat: add auth

--- app.py
+++ app.py
@@ -7,12 +7,52 @@ from servitium_cognitionis.llms import LLMFamily
 
 # module imports from the standard python environment
 import os
+import hmac
 import time
 import uuid
 import vertexai
 import google.auth
 import streamlit as st
 from langchain_core.messages import HumanMessage, AIMessage
+
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
+
+################################################################################
 
 def maybe_st_initialize_state():
     if "llm_families" not in st.session_state:
