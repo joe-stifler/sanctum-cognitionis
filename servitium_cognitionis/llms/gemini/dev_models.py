132eac5c89544b2d7fc7634489c7324e12360087
Jose Ribeiro <jose.neto23@imperial.ac.uk>
2024-05-19 03:43:16 +0100

    feat: increase timeout

--- servitium_cognitionis/llms/gemini/dev_models.py
+++ servitium_cognitionis/llms/gemini/dev_models.py
@@ -102,7 +102,7 @@ class GeminiDevBaseModel(LLMBaseModel):
             messages,
             stream=True,
             request_options={
-                "timeout": 60 * 4,
+                "timeout": 60 * 10,
             }
         )
 
