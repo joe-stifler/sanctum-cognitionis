3577d654db3f40d46770558eecd1d372da3c13f1
Jose Ribeiro <jose.neto23@imperial.ac.uk>
2024-06-06 12:41:32 +0100

    feat: improve app ui

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ from streamlit_extras.stylable_container import stylable_container
 logger = setup_logger()
 
 
-################################################################################
 def main():
     if "start_new_conversation" not in st.session_state:
         st.session_state["start_new_conversation"] = False
@@ -31,80 +30,87 @@ def main():
     notion_url = st.query_params.get("notion_url")
     notion_depth = st.query_params.get("notion_depth")
 
-    with stylable_container(
-        key="main_container",
+    parent_chat_container = stylable_container(
+        key="chat_container",
         css_styles="""
             {
                 left: 0;
-                bottom: 10px;
-                width: 100%;
+                top: 60px;
+                margin-bottom: 20vh;
+                width: 100vw;
                 position: fixed;
-                overflow-y: auto;
                 max-height: 100vh;
-                overflow-x: hidden;
                 padding-left: 10vw;
                 padding-right: 10vw;
-                padding-bottom: 25px;
+                padding-bottom: 130px;
             }
     """,
-    ):
-        parent_chat_container = stylable_container(
-            key="chat_container",
-            css_styles="""
-                {
-                    margin-top: 60px;
-                    height: calc(100% - 90px);
-                    max-height: calc(93vh - 90px);
-                }
+    )
+
+    with stylable_container(
+        key="footer_container",
+        css_styles="""
+            {
+                position: fixed;
+                bottom: 0;
+                width: 100vw;
+                z-index: 1000;
+                left: 0;
+                padding-left: 10vw;
+                padding-right: 10vw;
+                padding-bottom: 20px;
+            }
+            div[data-testid="stPopover"] {
+                min-width: 50px;
+                max-width: 7vw;
+            }
+            div[data-baseweb="popover"] {
+                min-width: 35vw;
+            }
+            div[data-testid="stChatInput"] * {
+                max-height: 200px;
+            }
         """,
-        )
+    ):
+        columns = st.columns([1, 1, 8])
+        notion_popover = columns[0].popover("📝", use_container_width=True)
+        files_popover = columns[1].popover("📎", use_container_width=True)
 
-        with stylable_container(
-            key="footer_container",
-            css_styles="""
-                * {
-                    max-height: 200px;
-                }
-                div[data-testid="stPopover"] {
-                    min-width: 50px;
-                    max-width: 7vw;
-                }
-                div[data-baseweb="popover"] {
-                    min-width: 35vw;
-                }
-            """,
-        ):
-            columns = st.columns([1, 1, 8])
-            notion_popover = columns[0].popover("📝", use_container_width=True)
-            files_popover = columns[1].popover("📎", use_container_width=True)
+        with columns[2]:
+            user_input_message = st.chat_input("Type your message here...")
 
-            with columns[2]:
-                user_input_message = st.chat_input("Type your message here...")
-
-            with notion_popover:
-                selected_nodes = notion_search_and_select(
-                    user_input_message, notion_url, notion_depth, notion_token
-                )
-
-            with files_popover:
-                user_uploaded_files = file_uploader_fragment(user_input_message)
-
-            user_uploaded_files.extend(selected_nodes)
-
-        with parent_chat_container:
-            api_status = model_settings(
-                chat_connector, logger, google_token, persona_name, model_name
+        with notion_popover:
+            selected_nodes = notion_search_and_select(
+                user_input_message, notion_url, notion_depth, notion_token
             )
 
-            with st.container(height=10000, border=False):
-                if api_status:
-                    chat_history = render_chat_history(chat_connector, logger)
-                    chat_messages(
-                        chat_history, user_input_message, user_uploaded_files, logger
-                    )
-                else:
-                    warning_message = "⚠️ Set your Google API Key above. Follow the instructions in the [official website](https://makersuite.google.com/app/apikey) to create a new API Key."
-                    st.warning(warning_message)
+        with files_popover:
+            user_uploaded_files = file_uploader_fragment(user_input_message)
+
+        user_uploaded_files.extend(selected_nodes)
+
+    with parent_chat_container:
+        api_status = model_settings(
+            chat_connector, logger, google_token, persona_name, model_name
+        )
+
+        with st.container(height=1000, border=False):
+            if api_status:
+                chat_history = render_chat_history(chat_connector, logger)
+                chat_messages(
+                    chat_history, user_input_message, user_uploaded_files, logger
+                )
+            else:
+                warning_message = "⚠️ Set your Google API Key above. Follow the instructions in the [official website](https://makersuite.google.com/app/apikey) to create a new API Key."
+                st.warning(warning_message)
+
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 main()
